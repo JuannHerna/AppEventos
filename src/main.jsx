98822a6be0205c6df49b3bd8a24ae51dd9a8ac11
franciscog98 <franciscomanuel.gonzalez3@gmail.com>
2024-05-17 19:21:17 -0300

    feat: add tailwind and react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/algo",
+    element: <h1>Hola</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
