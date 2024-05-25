73361c2e5537b6f561d7c05647baa9d1f1f8fecb
Juanc <jh85733@gmail.com>
2024-05-25 12:42:01 -0300

    Borre los 2 h2 y puse un h1 y p

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ function App() {
       <Navbar />
       <div className="h-screen bg-gradient-to-b from-teal-900 to-teal-500">
             <section className="text-white h-1/4  flex flex-col justify-center items-center">
-              <h2 className='text-4xl font-bold text-center text-teal-90'>
-                En esta seccion puedes encontrar los mejores eventos..
-              </h2>
-              <h2 className='mt-6 text-4xl font-bold text-center text-teal-90'>
-                Destacados
-              </h2>
+                <h1 className='text-5xl font-bold text-center text-teal-90'> 
+                  Destacados...
+                </h1>
+                <p className="text-2xl font-bold text-center text-teal-90 mt-6">
+                  Esta seccion encontraras los mejores Eventos.
+                </p>
             </section>
             <div className='grid grid-cols-3 gap-4 p-5'>
               <Tarjeta dia={Hoy} />
